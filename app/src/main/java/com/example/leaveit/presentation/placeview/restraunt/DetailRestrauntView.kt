584014235623e069c2a66ffdc634812e496cf50c
centerfoward <jsu5438@naver.com>
2025-01-24 15:04:26 +0900

    [UI] DetailRestraunt UI 초기 구현

--- app/src/main/java/com/example/leaveit/presentation/placeview/restraunt/DetailRestrauntView.kt
+++ app/src/main/java/com/example/leaveit/presentation/placeview/restraunt/DetailRestrauntView.kt
@@ -6,10 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.bumptech.glide.Glide
 import com.example.leaveit.databinding.FragmentDetailrestrauntviewBinding
+import com.example.leaveit.presentation.placeview.place.selectregionview.SelectRegionViewModel
 
 class DetailRestrauntView : Fragment() {
     lateinit var binding: FragmentDetailrestrauntviewBinding
+    private val rootViewModel: SelectRegionViewModel by activityViewModels()
     private val viewModel: RestrauntViewModel by activityViewModels()
 
     override fun onCreateView(
@@ -25,6 +28,26 @@ class DetailRestrauntView : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initView()
+        initTopAppBarText()
+
+    }
+
+    private fun initView(){
+        val data = viewModel.selectRestrauntData.value
+
+        // 이미지 설정
+        Glide.with(binding.root)
+            .load(data!!.image)
+            .fitCenter()
+            .into(binding.placeImage)
+
+
+
+    }
+
+    private fun initTopAppBarText(){
+        rootViewModel.setTopTapContent(viewModel.selectRestrauntData.value!!.title)
     }
 
     companion object {
